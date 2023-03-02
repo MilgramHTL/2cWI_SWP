638c56db84ea03a6f26a1d8583ff0d0fb890e458
Lukas <lukas.wagner@student.htldornbirn.at>
2023-03-02 09:29:57 +0100

    02_JS_Strings&Zahlen_finished

---
+++ 02_JS_Strings&Zahlen/script.js
@@ -0,0 +1,11 @@
+let a = 17.4;
+let b = 3.9;
+
+let sum = (a/b);
+
+console.log(sum);
+
+let firstname = ("Lukas");
+let lastname = ("Wagner");
+
+console.log("Ich heiße " + firstname + " " + lastname +".");
