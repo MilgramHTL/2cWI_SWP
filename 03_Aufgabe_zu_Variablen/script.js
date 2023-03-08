7558aaef046d1e0e97893eb6f0946b9823f2b47a
Lukas <lukas.wagner@student.htldornbirn.at>
2023-03-08 08:59:46 +0100

    03_finsished

--- 03_Aufgabe_zu_Variablen/script.js
+++ 03_Aufgabe_zu_Variablen/script.js
@@ -1,9 +1,25 @@
-let banana = banana;
-let apple = apple;
+let banana = "Banane";
+let apple = "Apple";
 let bananaPricePerKilo = 2.14;
 let applePricePerKilo = 3.43;
-let appleweight = 0.34;
-let bananaweight = 0.22;
+let appleWeight = 0.34;
+let bananaWeight = 0.22;
 
-console.log((1/appleweight));
-console.log((1/bananaweight));
+let applePerKilo = 1 / appleWeight;
+let bananaPerKilo = 1 / bananaWeight;
+
+let pricePerApple = applePricePerKilo / (1 / appleWeight);
+let pricePerBanana = bananaPricePerKilo / (1 / bananaWeight);
+let priceEigtApples = pricePerApple * 8;
+let priceElevenBananas = pricePerBanana * 11; 
+let oneTonApple = applePricePerKilo * 1000;
+let oneTonBananas = bananaPricePerKilo * 1000;
+
+console.log(applePerKilo);
+console.log(bananaPerKilo);
+console.log(pricePerApple);
+console.log(pricePerBanana);
+console.log(priceEigtApples);
+console.log(priceElevenBananas);
+console.log(oneTonApple);
+console.log(oneTonBananas);
