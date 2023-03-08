711939f7d2cf0b5cdf0fa5dc2d85e451d22d2b4d
Lukas <lukas.wagner@student.htldornbirn.at>
2023-03-08 09:35:12 +0100

    started_04_loops_and_finished_03

---
+++ 04_Schleifen/script.js
@@ -0,0 +1,23 @@
+for(let firstCount = 7; firstCount < 46; firstCount++){
+    console.log(firstCount);
+}
+
+for(let secondCount = 9; secondCount < 100; secondCount++){
+    console.log(secondCount);
+}
+
+for(let thirdCount = 10; thirdCount > -1; thirdCount--){
+    console.log(thirdCount);
+}
+
+for(let fourthCount = 1234; fourthCount > 1206; fourthCount--){
+    console.log(fourthCount);
+}
+
+let name = "Lukas";
+for(let countName = 0; countName >= 8; countName++){
+    console.log(name);
+}
+
+
+
