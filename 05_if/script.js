369194d933b516c467a83be9cf72b58af66f17cc
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2023-04-12 08:53:50 +0200

    added if statements

---
+++ 05_if/script.js
@@ -0,0 +1,27 @@
+let number = Math.floor(Math.random() * 100);
+
+if (number < 20) {
+    console.log("Mini");
+}
+
+if (number >= 20 && number < 50) {
+    console.log("Medium");
+}
+
+if (number > 50) {
+    console.log("Large");
+}
+
+let number2 = Math.floor(Math.random() * 100);
+
+if (number2 < 7) {
+    console.log("Mini");
+}
+
+if (number2 >= 7 && number2 < 90) {
+    console.log("Medium");
+}
+
+if (number2 > 90) {
+    console.log("Large");
+}
